33f79449e4b8ed1699abffe4a3bdda37ac275224
Randall D Williams <randallw1996@gmail.com>
2019-11-15 04:38:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,20 +13,20 @@ naming_system = {
      sub_category: {
        label: "Class",
        sub_category: {
-        label: "Order"
- }
- 
- PORTION_3 = {
-   label: "Family",
-   sub_category: {
-     label: "Genus",
+        label: "Order",
+        sub_category: {
+       label: "Family",
+       sub_category: {
+       label: "Genus",
+       sub_category: {
+       label: "Species"
+            }
+         }
+        }
+       }
+     }
    }
- }
- 
- PORTION_4 = {
-   label: "Species",
-   sub_category: nil
- }
+}
 
   # Remember:
   #  Kingdom
