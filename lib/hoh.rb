d4f0a08c5108fcbd2baeac6b20551179c764c637
Randall D Williams <randallw1996@gmail.com>
2019-11-15 03:40:42 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++
@@ -1,6 +0,0 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
